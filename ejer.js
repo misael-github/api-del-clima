7488160b796d1afbee168a53ae1829fbe8085310
Misael <dmisael2000@gmail.com>
2021-06-30 19:43:04 -0300

    agrego input, boton y lo selecciono en el archivo js

--- ejer.js
+++ ejer.js
@@ -1,22 +1,30 @@
+let div = document.querySelector("#temperatura");
+function peticionClima(json) {
+  let ciudad = document.querySelector("#user-city").value;
+  document.querySelector("#city-display").textContent = ciudad;
 
-    let div = document.querySelector("#temperatura")
-    function peticionClima(json){
-        console.log(json)
-       const temperatura = div.textContent = json.main.temp
-       document.querySelector("#temperatura").innerHTML = `${temperatura} °C`
-       const tempmin =  document.querySelector("#temperaturamaxima").textContent = json.main.temp_max
-       document.querySelector("#temperaturamaxima").innerHTML = `${tempmin} °C`
-       const tempmax =  document.querySelector("#temperaturaminima").textContent = json.main.temp_min
-       document.querySelector("#temperaturaminima").innerHTML = `${tempmax} °C`
-        const humedad = document.querySelector("#humedad").textContent = json.main.humidity
-        document.querySelector("#humedad").innerHTML = `${humedad}%`
-        const visibilidad = document.querySelector("#visibilidad").textContent = json.visibility
-        document.querySelector("#visibilidad").innerHTML = `${visibilidad} mts`
-     
-    }
-    // div.addEventListener("mouseenter", peticionClima )
-     
-//                                                           ciudad      &units=metric para cambio a grados y id de la api
-    fetch("https://api.openweathermap.org/data/2.5/weather?q= buenos aires&units=metric&appid=6245e636fa5b56f199a78e635b7010d0")
-    .then(response=> response.json())// cuando responde la api lo transforma en json
-    .then(json => peticionClima(json)) // a mi funcion le asigno el json
+  const temperatura = (div.textContent = json.main.temp);
+  document.querySelector("#temperatura").innerHTML = `${temperatura} °C`;
+  const tempmin = (document.querySelector("#temperaturamaxima").textContent =
+    json.main.temp_max);
+  document.querySelector("#temperaturamaxima").innerHTML = `${tempmin} °C`;
+  const tempmax = (document.querySelector("#temperaturaminima").textContent =
+    json.main.temp_min);
+  document.querySelector("#temperaturaminima").innerHTML = `${tempmax} °C`;
+  const humedad = (document.querySelector("#humedad").textContent =
+    json.main.humidity);
+  document.querySelector("#humedad").innerHTML = `${humedad}%`;
+  const visibilidad = (document.querySelector("#visibilidad").textContent =
+    json.visibility);
+  document.querySelector("#visibilidad").innerHTML = `${visibilidad} mts`;
+}
+const btnBuscar = document.querySelector("#buscar");
+btnBuscar.addEventListener("click", peticionClima);
+
+// div.addEventListener("mouseenter", peticionClima )
+//ciudad      &units=metric para cambio a grados y id de la api
+fetch(
+  `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&units=metric&appid=6245e636fa5b56f199a78e635b7010d0`
+)
+  .then((response) => response.json()) // cuando responde la api lo transforma en json
+  .then((json) => peticionClima(json)); // a mi funcion le asigno el json
