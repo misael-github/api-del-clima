207f2fc4e981b3a42a356b95227745f033677831
Misael <dmisael2000@gmail.com>
2021-07-01 09:11:29 -0300

    arreglo de javascript entero

--- ejer.js
+++ ejer.js
@@ -1,18 +1,31 @@
-const btnBuscar = document.querySelector("#buscar");
-btnBuscar.addEventListener("click", peticionClima);
+  const btnBuscar = document.querySelector("#buscar");
+  let div = document.querySelector("#temperatura");
+  const form = document.querySelector("form");
+  let temperatura = document.querySelector("#temperatura");
+  
+  btnBuscar.addEventListener("click", (e) => {
+    e.preventDefault();
+    let ciudad = form.querySelector("input").value;
+    document.querySelector("#city-display").textContent = ciudad;
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&units=metric&appid=6245e636fa5b56f199a78e635b7010d0`
+    )
+      .then((response) => response.json()) // cuando responde la api lo transforma en json
+      .then((json) => peticionClima(json)) // a mi funcion le asigno el json
+      .catch((error) => {
+          alert("Esta ciudad no se encuentra")
+      })
+  });
 
-fetch(
-  `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&units=metric&appid=6245e636fa5b56f199a78e635b7010d0`
-)
-  .then((response) => response.json()) // cuando responde la api lo transforma en json
-  .then((json) => peticionClima(json)); // a mi funcion le asigno el json
+
 
 
 function peticionClima(json) {
   let ciudad = document.querySelector("#user-city").value;
   document.querySelector("#city-display").textContent = ciudad;
   
-  const temperatura = (div.textContent = json.main.temp);
+
+  const temperatura = (json.main.temp);
   document.querySelector("#temperatura").innerHTML = `${temperatura} °C`;
 
   const tempmin = (document.querySelector("#temperaturamaxima").textContent =
@@ -31,8 +44,6 @@ function peticionClima(json) {
     json.visibility);
   document.querySelector("#visibilidad").innerHTML = `${visibilidad} mts`;
 }
-
 // div.addEventListener("mouseenter", peticionClima )
 
 //ciudad      &units=metric para cambio a grados y id de la api
-
